e0a8a622746df746ab5bfe6c778a4eb292f00426
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-19 03:32:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,3 +12,22 @@ class Book
 
 end
 
+require 'pry'
+
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  def initialize(title)
+    @title = title
+  end
+
+  GENRES = []
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre unless GENRES.any? { |g| g == genre }
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
